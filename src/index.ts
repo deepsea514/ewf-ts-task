615b05c849b35a496f7b9438c7c3cb3582d3f1e0
Wolf Warrier <wolfwarrier14@gmail.com>
2023-04-25 11:33:11 -0400

    pass the test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { Genre, Movie } from "./index.types"
+import { Genre, Movie } from "./index.types";
+import { movies } from "./db.json";
+
+const getRandomNumber = (limit: number): number => {
+  return Math.floor(Math.random() * limit);
+};
 
 export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
-  return []
-}
+  if (genres.length == 0) return [movies[getRandomNumber(movies.length)]];
+
+  let result = movies
+    .map((movie: Movie) => {
+      const isMatch = movie.genres.every(
+        (genre) => genres.find((_genre) => genre == _genre) != undefined
+      );
+      return {
+        movie,
+        isMatch,
+      };
+    })
+    .filter((value) => value.isMatch)
+    .map((val) => val.movie)
+    .sort((a, b) => b.genres.length - a.genres.length);
+  return result;
+};
